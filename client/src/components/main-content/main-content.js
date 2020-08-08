748dca8ecc03ab9484e6309c3a29d59c2fbb34c4
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-08-08 09:44:48 -0600

    row, nav, main-content outline

---
+++ client/src/components/main-content/main-content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Nav from '../nav/nav';
+import Row from '../row/row';
+import Column from '../column/column';
+import Card from '../card/card';
+import Container from '../container/container';
+
+class Main extends Component {
+    state = {
+
+    }
+}
+
+export default Main;
---
+++ client/src/components/row/row.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Row() {
+    return (
+
+    )
+}
+
+export default Row;
