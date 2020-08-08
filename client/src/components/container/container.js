1afa91031bb9a27639f808cb1e0f2534adf84031
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-08-08 09:56:53 -0600

    container.js

--- client/src/components/container/container.js
+++ client/src/components/container/container.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function Container() {
-    return (
-
-    )
+  return <div className="container">{props.children}</div>;
 }
 
 export default Container;
