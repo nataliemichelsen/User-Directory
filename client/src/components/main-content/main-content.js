43e78c26064b01988e9017c03100055b67cd4dac
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-08-10 10:26:03 -0600

    main-content.js 4 of 5

--- client/src/components/main-content/main-content.js
+++ client/src/components/main-content/main-content.js
@@ -11,70 +11,121 @@ import Container from "../container/container";
 
 // class for entire page - connects other components
 class Main extends Component {
-  state = {
-    resultsAll: [],
-    sortedResults: [],
-    results: [],
-    sorted: false,
-    name: false,
-    age: false,
-  };
+    state = {
+        resultsAll: [],
+        sortedResults: [],
+        results: [],
+        sorted: false,
+        name: false,
+        age: false,
+    };
 
-  // component mounting
-  componentDidMount() {
-    const arrow = document.getElementById("arrow");
-    arrow.style.display = "none";
+    // component mounting
+    componentDidMount() {
+        const arrow = document.getElementById("arrow");
+        arrow.style.display = "none";
 
-    API.find().then((res) => {
-      this.setState({
-        resultsAll: res.data.results,
-        sortedResults: res.data.results,
-        results: res.data.results,
-      });
-    });
-  }
-
-  // submit functionality
-  submitFunction = (event) => {
-    // define value & results & sorting using previously declared states ^
-    const value = event.target.value;
-    const resultsAll = this.state.resultsAll;
-    const sortedResults = this.state.sortedResults;
-
-    // filter & sort array
-    let filtered;
-    let array;
-
-    // sorting
-    if (!this.state.sortedResults) {
-      array = resultsAll;
-    } else {
-      array = sortedResults;
+        // target API then set states of results / sorting
+        API.find().then((res) => {
+            this.setState({
+                resultsAll: res.data.results,
+                sortedResults: res.data.results,
+                results: res.data.results,
+            });
+        });
     }
-    // timeout to search by phone, name, or dob
-    setTimeout(() => {});
-  };
 
-  // dropdown (sort) functionality
-  dropdownFunction = (event) => {
-    // define consts for this function
-    const value = event.target.innerText.trim();
-    const sortButton = document.getElementById("sortButton");
-    const arrow = document.getElementById("arrow");
-    if (value !== "By Default") {
-      let sorted;
-    }
-  };
+    // submit functionality
+    submitFunction = (event) => {
+        // define value & results & sorting using previously declared states ^
+        const value = event.target.value;
+        const resultsAll = this.state.resultsAll;
+        const sortedResults = this.state.sortedResults;
+
+        // filter & sort array
+        let filtered;
+        let array;
+
+        // sorting
+        if (!this.state.sortedResults) {
+            array = resultsAll;
+        } else {
+            array = sortedResults;
+        }
+        // timeout to search by phone, name, or dob
+        setTimeout(() => {
+            if (value) {
+                
+            } else {
+
+            }
+        });
+    };
+
+    // dropdown (sort) functionality
+    dropdownFunction = (event) => {
+        // define consts for this function
+        const value = event.target.innerText.trim();
+        const sortButton = document.getElementById("sortButton");
+        const arrow = document.getElementById("arrow");
+        // sort by...
+        // default 
+        if (value !== "By Default") {
+            let sorted;
+        // name
+        if (value !== 'By Name') {
+            
+        }
+        // age
+        if (value !== 'By Age') {
+            
+        }
+        // ascending order
+        if () {
+            
+        }
+        // descending order
+        if () {
+            
+        }
+        }
+    };
+
+    // rendering components data
+render() {
+    // setting results const to results state
+    const results = this.state.results;
+
+    // returning all components being pulled in
+    return (
+        <div className='main-content'>
+            <Nav
+                submitFunction={this.submitFunction}
+                dropdownFunction={this.dropdownFunction} />
+            <Container>
+                <Row>
+                    {
+                        results.map((employees, x) => (
+                            <Column key={x}>
+                                <Card
+                                    image={employees.picture}
+                                    firstName={employees.name.first}
+                                    lastName={employees.name.last}
+                                    age={employees.dob.age}
+                                    DOB={employees.dob.date}
+                                    phone={employees.phone}
+                                    email={employees.email}
+                                />
+                            </Column>
+                        ))
+                    }
+                </Row>
+            </Container>
+        </div>
+    )
+}
 }
 
-// rendering components data
-    render () {
-        const results = this.state.results;
 
-        // returning all components being pulled in
-        return (
-
-        )
-    }
 
 export default Main;
