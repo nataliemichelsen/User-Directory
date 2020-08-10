35a9465a882c478887b7f6a919b32a741607ee82
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-08-10 10:51:02 -0600

    removed app.js & updated index.js for page display

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
+// import react
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+// importing style - removed app.js & replaced it with components below
+import './index.css';
+
+// replacing app.js with components for page display
+import Main from '../src/components/main-content/main-content';
+import Footer from '../src/components/footer/footer';
+
+// must add components to render ^
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  [
+  <Main key='1' />, 
+  <Footer key='2' />
+  ],
   document.getElementById('root')
 );
 
+// removed this option
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
