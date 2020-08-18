b3483f55faf93df12b3d073ef76c52c0fbfdd93e
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-08-17 21:51:40 -0600

    build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MainContent from "./components/main-content/Main-Content.js"
+import MainContent from "./components/main-content/main-content.js"
 
 
 function App() {
