7293b0f982c7d89ca5dde52848f1ec8debc6160e
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-08-17 19:13:07 -0600

    homepage github

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-// import MainContent from "./components/main-content/Main-Content"
-import MainContent from "../src/components/main-content/Main-Content"
-
+import MainContent from "./components/main-content/Main-Content"
 
 function App() {
   return <MainContent />
