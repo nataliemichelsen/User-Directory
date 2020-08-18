3600d9d8dfdf69d62f9c744a04da868c4c6c2de0
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-08-17 18:46:28 -0600

    package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import MainContent from "../src/components/main-content/Main-Content"
-// import Card from './components/card/Card';
+import MainContent from "./components/main-content/Main-Content"
 
 function App() {
   return <MainContent />
