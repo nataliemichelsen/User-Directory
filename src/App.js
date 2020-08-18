4bc4102b82a2b296206dd45d076b1c47efabbcd7
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-08-17 19:15:27 -0600

    src

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import MainContent from "./components/main-content/Main-Content"
+import MainContent from "../src/components/main-content/Main-Content"
+
 
 function App() {
   return <MainContent />
